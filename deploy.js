394b62db8fe599f13e0afacb241edc1d250263a9
kv <konstantin.valeev@wirecard.com>
2022-06-30 21:34:44 +0800

    updated deploy script

--- deploy.js
+++ deploy.js
@@ -3,10 +3,8 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
+  'drum rural shift imitate title talent switch cupboard print kid update helmet',
+  'https://rinkeby.infura.io/v3/f2d9f126adef4e808467bd68d8774d3e'
 );
 const web3 = new Web3(provider);
 
