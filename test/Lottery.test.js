68b94168e547220df7a36fb674f24cb20e199122
kv <konstantin.valeev@wirecard.com>
2022-07-01 17:18:57 +0800

    tests added

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -17,7 +17,24 @@ beforeEach( async () => {
 })
 
 describe('Lottery contract', () => {
+
     it('deploys a contract', () => {
         assert.ok(lottery.options.address);
     });
+
+    it('allows one account to enter', async () => {
+        await lottery.methods.enter().send({
+            from: account[0], 
+            value: web3.utils.toWei('0.02', 'ether');
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equals(account[0], players[0]);
+        assert.equals(1, players.length);
+
+    });
+
 });
