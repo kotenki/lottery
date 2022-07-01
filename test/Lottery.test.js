3e5c4b12e4f080fb04fb158ee7447ff47c224bc3
kv <konstantin.valeev@wirecard.com>
2022-07-01 17:27:23 +0800

    test for multiple accts

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -37,4 +37,31 @@ describe('Lottery contract', () => {
 
     });
 
+    it('allows multiple accounts to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0], 
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[1], 
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[2], 
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[2]);
+        assert.equal(accounts[1], players[2]);
+        assert.equal(3, players.length);
+
+    });
+
 });
